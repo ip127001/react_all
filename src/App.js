25b566457bd0a1d40203d156145b236e0737453a
Rohit Kumawat <rkumawat125@gmail.com>
2019-02-18 19:43:16 +0530

    implemented props and children property

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app.</h1>
         <p>this is working</p>
-        <Person />
+        <Person name="Rohit" age="22" />
+        <Person name="mk" age="00">Go for shopping<p>hi there</p></Person>
+        <Person name="Mohit" age="19" />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a react app'));
--- src/Person/person.js
+++ src/Person/person.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-const person = () => {
-    return <p>Hi there i am new component</p>;
+const person = (props) => {
+    return (
+        <div>
+            <p>Hi there i am new component</p>
+            <p>{props.name}</p>       
+            <p>{props.age}</p>
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person;
