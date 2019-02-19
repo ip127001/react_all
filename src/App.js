abdee1450313b933da7e9d2593968e54b52bf964
Rohit Kumawat <rkumawat125@gmail.com>
2019-02-19 11:46:44 +0530

    used state property

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'rohit', age: 22},
+      {name: 'mk', age: 0},
+      {name: 'mohit', age: 19}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app.</h1>
         <p>this is working</p>
-        <Person name="Rohit" age="22" />
-        <Person name="mk" age="00">Go for shopping<p>hi there</p></Person>
-        <Person name="Mohit" age="19" />
+        <button>Change something</button>
+        <a href="https://www.sejda.com/mgl/compress-pdf-online">Compress PDF</a>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} /><br></br>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Go for shopping<p>hi there</p></Person><br></br>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a react app'));
