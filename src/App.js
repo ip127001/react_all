e017a7a20edb597ef2cd604fd8e4ede292344d80
Rohit Kumawat <rkumawat125@gmail.com>
2019-02-23 10:39:03 +0530

    flexible list with input changes (two way binding)

--- src/App.js
+++ src/App.js
@@ -25,14 +25,20 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
 
-  valueSwitchMethod = (event) => {
-    this.setState({
-      persons: [
-        {name: event.target.value, age: 22},
-        {name: 'mk', age: 0},
-        {name: 'mohit', age: 19}
-      ],
-    })
+  valueSwitchMethod = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
 
   render() {
@@ -54,6 +60,7 @@ class App extends Component {
               name={person.name}
               age={person.age}
               key={person.id}
+              changed={(event) => this.valueSwitchMethod(event, person.id)}
               />
           })}
         </div>
