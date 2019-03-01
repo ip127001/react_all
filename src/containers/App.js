0ad19b40ad9d1e2133c2f523c35bd12bf7bd01f3
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-01 13:56:39 +0530

    implemented 'component update lifecycle for state changes'

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,6 +29,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');
+  }
+
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
