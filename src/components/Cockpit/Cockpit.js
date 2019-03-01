cdd5fd8818890fc4f7bd5b6a6c5fb678d417feca
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-01 14:12:08 +0530

    used useEffect() function in functional components as react hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        setTimeout(() => {
+            alert('saved to the cloud');
+        }, 1000);
+    }, [props.persons])
+
     const classe = [];
     let btnClass = '';
 
