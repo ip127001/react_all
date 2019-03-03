4fcd1186ee208233f4e3cd7e7bcd71b680bb2c56
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-03 12:28:23 +0530

    rendered adjacent jsx elements using hoc(Aux)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
+import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <div className={classes.Person}>
-                <p>Hi there i am new component</p>
+            <Aux>
                 <p onClick={this.props.click}>I am {this.props.name} of age {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} defaultValue={this.props.name}></input>
-            </div>  
-    )
+            </Aux>
+        )
     }
 }
 
