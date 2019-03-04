9a01fae77c40bf3dc9234132f23f37cebf5e3066
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-04 16:40:36 +0530

    using refs in class based component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,13 +6,22 @@ import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...')
         return (
             <Aux>
                 <p onClick={this.props.click}>I am {this.props.name} of age {this.props.age}</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} defaultValue={this.props.name}></input>
+                <input type="text" ref={this.inputElementRef} onChange={this.props.changed} defaultValue={this.props.name}></input>
             </Aux>
         )
     }
